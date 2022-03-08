3d2cbf82658f4816da25afdced7b9d772343597b
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2022-03-08 00:24:06 +0000

    Add files via upload

---
+++ shapes-3D.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <cmath>
+using namespace std;
+
+void cube() {
+	double cube_sides;
+	double cubeVolume;
+	cout << "Enter side of cube: ";
+	cin >> cube_sides;
+	cubeVolume = pow(cube_sides, 3);
+	cout << "Volume of cube with sides, " << cube_sides << "units, is: " << cubeVolume << " cubic units" << endl;
+}
+
+void cuboid() {
+	double cuboid_length;
+	double cuboid_width;
+	double cuboid_height;
+	double cuboidVolume;
+	cout << "Enter length of cuboid: ";
+	cin >> cuboid_length;
+	cout << "Enter width of cuboid: ";
+	cin >> cuboid_width;
+	cout << "Enter height of cuboid: ";
+	cin >> cuboid_height;
+	cuboidVolume = cuboid_length * cuboid_width * cuboid_height;
+	cout << "Volume of cuboid with length, " << cuboid_length << "units, width, " << cuboid_width << "units and height, " << cuboid_height << "units, is: " << cuboidVolume << " cubic units." << endl;
+}
